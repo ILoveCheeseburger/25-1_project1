3d6a51c582afdda8b3164180e17c2cc652811bcd
YourGitHubUsername <YourGitHubEmail@example.com>
2025-03-26 23:00:28 +0900

    main.c 리스트 명령어 처리 완료

--- 20201563/main.c
+++ 20201563/main.c
@@ -28,6 +28,16 @@ int find_list_index(const char *name)
     return -1;
 }
 
+bool cmp_list_data(const struct list_elem *a, const struct list_elem *b, void *aux) // sort 에서 사용하는 리스트내의 값 비교함수
+{
+    list_item *item_a = list_entry(a, list_item, elem);
+    list_item *item_b = list_entry(b, list_item, elem);
+    if (item_a->data < item_b->data)
+        return true;
+    else
+        return false;
+}
+
 void process_command(char *line)
 {
     char cmd[32], arg1[32], arg2[32], arg3[32];
@@ -115,6 +125,148 @@ void process_command(char *line)
     }
 
     printf("Unknown command: %s\n", line);
+
+    if (strcmp(cmd, "list_shuffle") == 0) // list_shuffle 구현
+    {
+        int idx = find_list_index(arg1);
+        if (idx == -1)
+        {
+            printf("List not found.\n");
+            return;
+        }
+
+        // 리스트 길이 계산
+        int listSize = 0;
+        struct list_elem *e;
+        for (e = list_begin(&my_lists[idx]); e != list_end(&my_lists[idx]); e = list_next(e))
+            listSize++;
+
+        // 요소들을 배열에 저장 (임시배열임. 리스트 각 노드의 주소를 담고 있는 리스트임. )
+        // 이론상 이 배열을 쓰지 않고 리스트 자체에서 접근하여 섞을 수도 있지만, 리스트가 다음 노드의 포인터를 지니고 있는 식으로 연결이 되어있기 때문에
+        // 해당 노드의 값으로 접근하려면 계속 순회를 해서 접근해야해서 시간 복잡도가 오래걸림. 그래서
+        // 해당 노드들의 주소값을 갖고있는 배열을 만들어서, 더 쉽게 접근하도록 하는 거임.
+        list_item *items[listSize];
+        int i = 0;
+        for (e = list_begin(&my_lists[idx]); e != list_end(&my_lists[idx]); e = list_next(e))
+            items[i++] = list_entry(e, list_item, elem); // list_entry는 정확히 한 단계만 올라가는 거임. 노드안에 elem 구조체가 있는데. 그 구조체의 원소를 가지고, 한 단계 올라가서, 그 노드의 주소를 반환하는거임.
+
+        // Fisher-Yates 셔플 알고리즘
+        for (int i = listSize - 1; i > 0; i--)
+        {
+            int j = rand() % (i + 1);
+            int temp = items[i]->data;
+            items[i]->data = items[j]->data;
+            items[j]->data = temp;
+        }
+
+        return;
+    }
+
+    if (strcmp(cmd, "list_push_front") == 0)
+    {
+
+        int idx = find_list_index(arg1);
+        if (idx == -1)
+        {
+            printf("List not found.\n");
+            return;
+        }
+
+        list_item *item = malloc(sizeof(list_item));
+        int value = atoi(arg2);
+        item->data = value;
+
+        list_push_front(&my_lists[idx], &item->elem);
+        return;
+    }
+
+    if (strcmp(cmd, "list_insert") == 0) // list_insert mylist 2 99
+    {
+        int idx = find_list_index(arg1);
+        if (idx == -1)
+        {
+            printf("List not found.\n");
+            return;
+        }
+
+        int cnt = atoi(arg2);
+        int new_value = atoi(arg3);
+
+        struct list_elem *e = list_begin(&my_lists[idx]);
+
+        for (int i = 0; i < cnt; i++)
+        {
+            e = list_next(e);
+        }
+
+        list_item *item = malloc(sizeof(list_item));
+        item->data = new_value;
+
+        list_insert(e, &item->elem);
+        return;
+    }
+
+    if (strcmp(cmd, "list_remove") == 0) // list_remove mylist 2
+    {
+        int idx = find_list_index(arg1);
+        if (idx == -1)
+        {
+            printf("List not found.\n");
+            return;
+        }
+
+        int cnt = atoi(arg2);
+        struct list_elem *e = list_begin(&my_lists[idx]);
+
+        for (int i = 0; i < cnt; i++)
+        {
+            e = list_next(e);
+        }
+
+        struct list_elem *removed_e = list_remove(e);
+        list_item *removed_item = list_entry(removed_e, list_item, elem); // list_entry 의 인자는 세개임 끝에 두개는sizeof 처럼 타입 그 자체를 넣음.
+        free(removed_item);
+    }
+
+    if (strcmp(cmd, "list_max") == 0) // list_max mylist
+    {
+        int idx = find_list_index(arg1);
+        if (idx == -1)
+        {
+            printf("List not found.\n");
+            return;
+        }
+
+        int max = list_entry(list_begin(&my_lists[idx]), list_item, elem)->data;
+        // &my_list[idx]는 list* 이지, list_elem* 이 아님.
+        // list_entry 는 list_elem*을 참조하여, 그 elem를 갖고있는 리스트 칸을 꺼내는 아이니까
+        // 첫번쨰elem*로 바꿔줘야 하니까 list_begin() 사용
+        struct list_elem *e;
+        for (e = list_begin(&my_lists[idx]); e != list_end(&my_lists[idx]); e = list_next(e))
+        {
+            int now_data = list_entry(e, list_item, elem)->data;
+
+            if (now_data > max)
+            {
+                max = now_data;
+            }
+        }
+
+        printf("%d\n", max);
+    }
+
+    if (strcmp(cmd, "list_sort") == 0) // list_sort <list_name> : list_sort mylist
+    {
+        int idx = find_list_index(arg1);
+        if (idx == -1)
+        {
+            printf("List not found.\n");
+            return;
+        }
+
+        list_sort(&my_lists[idx], cmp_list_data, NULL);
+        return;
+    }
 }
 
 int main()
